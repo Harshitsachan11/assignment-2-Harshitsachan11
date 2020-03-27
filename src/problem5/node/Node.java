712b49b73fa2d4966b1baa6a508751640baa09ab
Harshit Sachan <harshit.sachan_cs18@gla.ac.in>
2020-03-27 13:34:38 +0530

    Main class created

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -4,7 +4,40 @@
  *  Date: 23-Mar-20
  *  Time: 7:06 PM
  */
-package problem5.node;
 // to define node properties
-public class Node {
+package problem5.node;
+import problem5.student.Student;
+// to define node properties
+public class Node
+{
+    private Student s;
+    private  Node next;
+
+
+    public Student getS() {
+        return s;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+
+    public void setS(Student s) {
+        this.s = s;
+    }
+
+    public Node getNext()
+    {
+        return next;
+    }
+
+
+
+    public Node(Student s)
+    {
+        this.s = s;
+        next=null;
+    }
+
 }
